1f48a42f4564f85021204c0d9ff4e35abb00457b
apekshachoudhary <rajendrachoudhary2028@gmail.com>
2022-10-06 21:07:22 +0530

    refactored code using statics & methods

--- database/user/index.js
+++ database/user/index.js
@@ -1,16 +1,30 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-    fullName: {type: String, require: true},
-    email: {type: String, require: true},
-    password: {type: String},
+    fullName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String },
     address: [{
-        details: {type: String},
-        for: {type: String}
+        details: { type: String },
+        for: { type: String }
     }],
-    phoneNumber: [{type: Number}],
-},{
+    phoneNumber: [{ type: Number }],
+}, {
     timestamps: true,
 });
+
+// statics and methods
+UserSchema.statics.findByEmailAndPhone = async ({ email, phoneNumber }) => {
+    
+    // check whether email exists
+    const checkUserByEmail = await UserModel.findOne({ email });
+    const checkUserByPhone = await UserModel.findOne({ phoneNumber });
+
+    if (checkUserByEmail || checkUserByPhone) {
+        throw new Error("User already exists!");
+    }
+
+    return false;
+}
 
 export const UserModel = mongoose.model('Users', UserSchema);
